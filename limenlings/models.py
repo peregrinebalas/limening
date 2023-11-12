5fbd8b2510a7009c1493ea7758dd51f9980c9340
Peregrine Balas <peregrinereedbalas@gmail.com>
2023-11-12 17:58:29 -0600

    add clade

--- limenlings/models.py
+++ limenlings/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 
-class Limenling(model.Model):
+class Limenling(models.Model):
     OMNIVERT = "O"
     AMBIVERT = "A"
     INTROVERT = "I"
@@ -15,7 +15,22 @@ class Limenling(model.Model):
     ]
 
     name = models.CharField(max_length=32)
+    clade = models.ForeignKey(Clade)
+    nlp_model = models.ForeignKey(NlpModel)
     max_speed = models.BigIntegerField
     vert = models.CharField(max_length=1, choices=VERT_CHOICES)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+class Clade(models.Model):
+    name = models.CharField(max_length=32)
+    # sapience = models.
+
+    def __str__(self):
+        return self.name
+
+class NlpModel(models.Model):
+    
