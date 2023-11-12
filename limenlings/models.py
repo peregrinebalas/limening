c3aea0614a0bb83a1225d54dbc8866bee8ea6b2a
Peregrine Balas <peregrinereedbalas@gmail.com>
2023-11-12 17:34:26 -0600

    limenlings model

--- limenlings/models.py
+++ limenlings/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+class Limenling(model.Model):
+    OMNIVERT = "O"
+    AMBIVERT = "A"
+    INTROVERT = "I"
+    EXTROVERT = "E"
+    VERT_CHOICES = [
+        (OMNIVERT, "Omnivert"),
+        (AMBIVERT, "Ambivert"),
+        (INTROVERT, "Introvert"),
+        (EXTROVERT, "extro")
+    ]
+
+    name = models.CharField(max_length=32)
+    max_speed = models.BigIntegerField
+    vert = models.CharField(max_length=1, choices=VERT_CHOICES)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
