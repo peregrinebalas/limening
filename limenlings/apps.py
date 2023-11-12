b54a632c790cce18c6e7f4e6a26a06c4b4ad062c
Peregrine Balas <peregrinereedbalas@gmail.com>
2023-11-12 17:34:12 -0600

    update to limenlings

--- limenlings/apps.py
+++ limenlings/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class LimeningsConfig(AppConfig):
+class LimenlingsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'limenings'
+    name = 'limenlings'
