b4ab6297e34e54ec04b4e815ac05a07d827b6f26
Peregrine Balas <peregrinereedbalas@gmail.com>
2023-11-29 22:43:52 -0600

    clade -> family, add comments

--- entities/models.py
+++ entities/models.py
@@ -2,13 +2,28 @@ from django.db import models
 from django.contrib.gis.geos import Point
 from django.contrib.gis.db.models.functions import Distance
 
-# Create your models here.
+
 class NlpModel(models.Model):
     source = models.CharField
     model_version = models.CharField
 
-class Clade(models.Model):
+# Family: (noun) A set of entities with common characteristics.
+# The Family model defines generic behaviors of each entity type.
+# When an entity is created, their "nature" is from their famiy,
+# which is then built on by their individual experiences.
+class Family(models.Model):
+    BILATERAL = "B"
+    RADIAL = "R"
+    ASYMMETRICAL = "A"
+    BODY_PLAN_CHOICES = [
+        (BILATERAL, "Bilateral"),
+        (RADIAL, "Radial"),
+        (ASYMMETRICAL, "Asymmetrical")
+    ]
     name = models.CharField(max_length=32)
+    body_plan = models.CharField(choices=BODY_PLAN_CHOICES)
+    base_form = models.ImageField
+    max_speed = models.BigIntegerField
     # sapience = models.CharField()
 
     def __str__(self):
@@ -22,7 +37,7 @@ class Portal(models.Model):
 
     def save(self, *args, **kwargs):
         self.point = Point(self.lng, self.lat)
-        super(Wall, self).save(*args, **kwargs)
+        super(Portal, self).save(*args, **kwargs)
 
     def order_by_dist(point):
         return Portal.objects.annotate(distance=Distance('point', point))
@@ -34,9 +49,8 @@ class Portal(models.Model):
     def __str__(self):
         return self.name
 
-# Entity personality fields:
-# vert - how outgoing the entity is. visible to user.
-# psyche projection - if the entity will take previous experiences from other users and approach a new user as though they will behave the same way. no visible to users.
+# Entity: (noun) a thing with distinct and independent existence.
+
 class Entity(models.Model):
     OMNIVERT = "O"
     AMBIVERT = "A"
@@ -50,11 +64,11 @@ class Entity(models.Model):
     ]
 
     name = models.CharField(max_length=32)
-    clade = models.ForeignKey(Clade)
+    family = models.ForeignKey(Family)
     nlp_model = models.ForeignKey(NlpModel)
     max_speed = models.BigIntegerField
-    vert = models.CharField(max_length=1, choices=VERT_CHOICES)
-    projection = models.BooleanField
+    vert = models.CharField(max_length=1, choices=VERT_CHOICES) # how outgoing the entity is. visible to user.
+    projection = models.BooleanField # if the entity will take previous experiences from other users and approach a new user as though they will behave the same way. not visible to users.
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     manifested_at = models.DateTimeField
