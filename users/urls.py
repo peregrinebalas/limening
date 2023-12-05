9b5f3095ee761afb255e2c769fc5e74c671ee1b2
Peregrine Balas <peregrinereedbalas@gmail.com>
2023-12-04 21:51:20 -0600

    update users endpoints

--- users/urls.py
+++ users/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = 'users'
 urlpatterns = [
     path('users', views.CreateUser.as_view(), name='user-create'),
-    path('users', views.UpdateUser.as_view(), name='user-update'),
+    path('users/<int:pk>', views.UpdateUser.as_view(), name='user-update'),
     path('users/<int:pk>', views.ShowUser.as_view(), name='user-detail'),
     path('users/<int:pk>', views.DeleteUser.as_view(), name='user-delete'),
 ]
--- users/views.py
+++ users/views.py
@@ -22,16 +22,16 @@ class CreateUser(generics.CreateAPIView):
     permission_classes = (permissions.AllowAny,)
 
     def post(self, request, *args, **kwargs):
-        username = request.data.get("username", None)
-        email = request.data.get("email", None)
-        password = request.data.get("password", None)
-        # first_name = request.data.get("first_name", None)
-        # last_name = request.data.get("last_name", None)
-        user = User.objects.create_user(username, password, email)
-        data = UserSerializer(instance=user, context={'request': request}).data
         try:
-            # user = User.objects.create_user(username, password, email)
-            return Response(data=data, status=status.HTTP_201_CREATED)
+            user = UserSerializer(data=request.data, context={'request': request}, partial=True)
+            print(user.is_valid())
+            if user.is_valid():
+                user.save()
+                return Response(user.data, status=status.HTTP_201_CREATED)
+            else:
+                return Response(data={
+                "error": "Fields missing, could not create user."
+                }, status=status.HTTP_400_BAD_REQUEST)
         except IntegrityError:
             return Response(data={
                 "error": "Fields missing, could not create user."
@@ -45,21 +45,16 @@ class UpdateUser(generics.UpdateAPIView):
     permission_classes = (permissions.AllowAny,)
 
     def patch(self, request, *args, **kwargs):
-        username = request.data.get("username", None)
-        email = request.data.get("email", None)
-        password = request.data.get("password", None)
-        first_name = request.data.get("first_name", None)
-        last_name = request.data.get("last_name", None)
         try:
             user = User.objects.get(pk=kwargs['pk'])
-            user.username=username
-            user.password=password
-            user.email=email
-            user.first_name=first_name
-            user.last_name=last_name
-            user.save
-
-            return Response(data=UserSerializer(user).data, status=status.HTTP_200_OK)
+            serializer = UserSerializer(user, data=request.data, context={'request': request}, partial=True)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(data=serializer.data, status=status.HTTP_200_OK)
+            else:
+                return Response(data={
+                "error": "Fields missing, could not create user."
+                }, status=status.HTTP_400_BAD_REQUEST)
         except IntegrityError:
             return Response(data={
                 "error": "Fields missing, could not save user."
